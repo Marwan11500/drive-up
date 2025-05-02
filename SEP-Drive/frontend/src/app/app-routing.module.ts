5ddc08f606ada1634fef12a36e27d0257da87619
Ziad Morsy <ziad.morsy1998@hotmail.com>
2025-05-02 19:17:10 +0200

    added profile page route

--- SEP-Drive/frontend/src/app/app-routing.module.ts
+++ SEP-Drive/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./profile/profile.module').then((m) => m.ProfileModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
