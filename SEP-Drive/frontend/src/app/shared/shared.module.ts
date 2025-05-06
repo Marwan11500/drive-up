b1f207f1edd0606e2a2811d641e9155c76c319d5
Ziad Morsy <ziad.morsy1998@hotmail.com>
2025-05-07 01:30:04 +0200

    implemented navbar, edited login and register

--- SEP-Drive/frontend/src/app/shared/shared.module.ts
+++ SEP-Drive/frontend/src/app/shared/shared.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
+import { LoginDialogComponent } from './components/login-dialog/login-dialog.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     InputComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginDialogComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbar,
+    MatButton,
+    RouterLink
   ],
   exports: [
     ButtonComponent,
