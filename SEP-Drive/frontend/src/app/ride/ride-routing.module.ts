917d7545720e4291ec90ca7c0b2a78f165365225
Ziad Morsy <ziad.morsy1998@hotmail.com>
2025-05-06 21:04:59 +0200

    minor fix

--- SEP-Drive/frontend/src/app/ride/ride-routing.module.ts
+++ SEP-Drive/frontend/src/app/ride/ride-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RideFormComponent } from './components/ride-form/ride-form.component';
 
-const routes: Routes = [
-  { path: 'ride-form', component: RideFormComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
