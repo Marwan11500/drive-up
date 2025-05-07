3dab3ae48eea62448350352dc95201f234c8017a
salmen <salmen.dridi@stud.uni-due.de>
2025-05-07 15:04:36 +0200

    routing for 2fa

--- SEP-Drive/frontend/src/app/app.module.ts
+++ SEP-Drive/frontend/src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { RideModule } from './ride/ride.module';
-
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './auth/register/register.component';
+import { TwoFactorComponent } from './auth/two-factor/two-factor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    TwoFactorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    RideModule
+    RideModule,
+    FormsModule
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi())
