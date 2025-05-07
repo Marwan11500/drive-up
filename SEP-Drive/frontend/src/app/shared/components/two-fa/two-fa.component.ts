1cb38648632b5ded88d4e84099f37ef3c7e7ab52
salmen <salmen.dridi@stud.uni-due.de>
2025-05-07 17:04:22 +0200

    add 2fa component

---
+++ SEP-Drive/frontend/src/app/shared/components/two-fa/two-fa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-fa',
+  standalone: false,
+  templateUrl: './two-fa.component.html',
+  styleUrl: './two-fa.component.scss'
+})
+export class TwoFaComponent {
+
+}
