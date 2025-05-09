7b0202f0d8685c6073411e8f043c3af8d266f55d
salmen <salmen.dridi@stud.uni-due.de>
2025-05-09 02:30:14 +0200

    added inputs in login dialog and 2fa dialog

--- SEP-Drive/frontend/src/app/shared/components/two-fa/two-fa.component.ts
+++ SEP-Drive/frontend/src/app/shared/components/two-fa/two-fa.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-two-fa',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './two-fa.component.scss'
 })
 export class TwoFaComponent {
+  verificationCode: string = '';
 
+  constructor(private dialogRef: MatDialogRef<TwoFaComponent>) {}
+
+  verifyCode() {
+    console.log('Eingegebener Code:', this.verificationCode);
+    // Hier kannst du die Code-Verifizierung gegen dein Backend machen
+    this.dialogRef.close(this.verificationCode); // gibt den Code zurück
+  }
 }
